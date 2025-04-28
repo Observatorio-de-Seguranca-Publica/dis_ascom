d679b72d34f0c7b49ac2b5cdb41c29a416efe011
henriquesiqr <henrique.ribeiro4488@gmail.com>
2025-04-28 15:50:48 -0300

    add script registros pichcao

---
+++ SQLcode/registro_pichacao.sql
@@ -0,0 +1,29 @@
+SELECT oco.numero_ocorrencia,
+                      oco.qtd_ocorrencia,
+                      oco.natureza_descricao,
+                      oco.natureza_consumado,
+                      CAST (oco.data_hora_fato as date) as data_fato,
+                      YEAR (oco.data_hora_fato) as ano_fato,
+                      MONTH (oco.data_hora_fato) as mes_numerico_fato,
+                      SUBSTRING(CAST(oco.data_hora_fato AS STRING), 12, 8) AS horario_fato,
+                      oco.motivo_presumido_descricao_longa,
+                      oco.instrumento_utilizado_descricao_longa,
+                      oco.local_imediato_longa,
+                      oco.complemento_natureza_descricao,
+                      oco.tipo_logradouro_descricao,
+                      oco.descricao_endereco,
+                      oco.nome_bairro,
+                      oco.nome_municipio,
+                      oco.codigo_municipio,
+                      oco.ocorrencia_uf,
+                      oco.unidade_responsavel_registro_nome,
+                      oco.numero_latitude,
+                      oco.numero_longitude,
+                      oco.tipo_local_descricao,
+                      oco.natureza_ind_consumado
+               FROM db_bisp_reds_reporting.tb_ocorrencia AS oco
+               WHERE oco.data_hora_fato >= '2023-01-01 00:00:00.000'
+               AND oco.data_hora_fato < '2025-03-01 00:00:00.000'
+               AND oco.ocorrencia_uf = 'MG'
+               AND oco.ind_estado IN ('F', 'R')
+               AND oco.natureza_codigo IN ('L05002', 'L29004', 'L30065')
